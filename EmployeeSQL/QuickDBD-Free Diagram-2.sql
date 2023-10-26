f6202538a22bdad85d1cf200aa7c4c66c265b4bf
Lovepreet008 <cheemalove592@gmail.com>
2023-10-26 00:55:55 -0400

    Adding Readme and ERD diagram

---
+++ EmployeeSQL/QuickDBD-Free Diagram-2.sql
@@ -0,0 +1,64 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/8NvMVA
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Departments" (
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Salaries" (
+    "emp_no" INTEGER   NOT NULL,
+    "salary" INTEGER   NOT NULL,
+    CONSTRAINT "pk_Salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Dept_emp" (
+    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" VARCHAR   NOT NULL,
+    "(emp_no," dept_no)   NOT NULL,
+    CONSTRAINT "pk_Dept_emp" PRIMARY KEY (
+        "(emp_no,"
+     )
+);
+
+CREATE TABLE "Employees" (
+    "emp_title_id" VARCHAR   NOT NULL,
+    "birth_date" VARCHAR   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "sex" VARCHAR   NOT NULL,
+    "hire_date" VARCHAR   NOT NULL,
+    "emp_no" INTEGER   NOT NULL
+);
+
+CREATE TABLE "Dept_manager" (
+    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" VARCHAR   NOT NULL,
+    "(emp_no," dept_no)   NOT NULL,
+    CONSTRAINT "pk_Dept_manager" PRIMARY KEY (
+        "(emp_no,"
+     )
+);
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Salaries" ("emp_no");
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Salaries" ("emp_no");
+
+ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Salaries" ("emp_no");
+
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # sql-challenge
 Module-9
+#schema.sql
+Create all the required tables from CSV files.
+There are total 5 tables departments, salaries, dept_emp, dept_manager, employees. 
+The departments table contains the dept_no private key.
+The salaries table contain the emp_no as a private key.
+
+Rest all the tables share the above foreign keys to create a relation.
+#queries.sql
+create a query and List the employee number, last name, first name, sex, and salary of each employee.
+
+List the first name, last name, and hire date for the employees who were hired in 1986.
+
+List the manager of each department along with their department number, department name, employee number, last name, and first name.
+
+List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+
+List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+
+List each employee in the Sales department, including their employee number, last name, and first name.
+
+List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+
+
+#ERD diagram
+Entity Relation Diagram created following the above creation details.
+
+The ERD exported to save as Schema for the tables.
+
+
+
